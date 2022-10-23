ac99ad14f90e88ff2e9ee839dced205f24cf0e3b
Eric Sison <ericsison.dev@gmail.com>
2022-10-23 15:13:59 +0800

    added data table component

---
+++ src/components/DataTable/DataTable.stories.tsx
@@ -0,0 +1,16 @@
+// import React from 'react';
+// import { ComponentStory } from '@storybook/react';
+
+// import { DataTable } from './DataTable';
+
+// export default {
+//   title: 'DataTable',
+//   component: DataTable
+// };
+
+// // create a template for button component
+// const Template: ComponentStory<typeof DataTable> = args => <DataTable {...args} />;
+
+// export const DataTableComponent = Template.bind({});
+
+// DataTableComponent.args = {};
