594d977a52f62bb2a2a2db9d4887c41605f8d68b
ericsison <ericsison.dev@gmail.com>
2022-12-06 16:46:38 +0800

    updated modal component

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,175 +1,107 @@
-import React, { createContext, ReactNode, useContext } from 'react';
-import { FunctionComponent, Fragment } from 'react';
-import { Dialog, Transition } from '@headlessui/react';
+import { Dialog } from '@headlessui/react';
+import { AnimatePresence, m, LazyMotion, domAnimation } from 'framer-motion';
+import React, { FunctionComponent, ReactNode, useState } from 'react';
 import {
-  modalBodyStyles,
-  modalFooterStyles,
-  modalHeaderStyles,
-  modalHeightStyles,
-  modalWidthStyles,
+  bodyClass,
+  childrenContainer,
+  footerClass,
+  headerClass,
+  overlayClass,
+  panelClass,
+  panelContainerClass,
 } from './Modal.styles';
-import { Close } from '../Close';
 
-/**
- *  set modal context to control its open and close state
- *  when composed with other components, such as the Header
- */
-type ModalContextState = {
-  open: boolean;
-  setOpen: (show: boolean) => void;
+export type Props = {
+  className?: string;
+  children: ReactNode | ReactNode[];
 };
 
-/**
- *  set default props for all other components
- *  composed with modal
- */
-type Props = {
-  children: ReactNode | Array<ReactNode>;
-};
-
-/**
- *  set header props
- */
-type HeaderProps = Props & {
-  withCloseBtn?: boolean;
-};
-
-/**
- *  set body props
- */
-type BodyProps = Props & {
-  scrollable?: boolean;
-};
-
-/**
- *  set footer props
- */
-type FooterProps = Props & {
-  alignEnd?: boolean;
-};
-
-/**
- *  compose modal component
- */
-type ModalComposition = {
-  Header: typeof Header;
-  Title: typeof Title;
-  Body: typeof Body;
-  Footer: typeof Footer;
-};
-
-/**
- *  set props for modal component
- */
 export type ModalProps = Props & {
   open: boolean;
   setOpen: (state: boolean) => void;
   size?: 'sm' | 'md' | 'lg' | 'xl' | 'full';
+  steady?: boolean;
+  fixedHeight?: boolean;
 };
 
-// initialize modal context
-const ModalContext = createContext({} as ModalContextState);
+type HeaderProps = Props & {
+  withCloseBtn?: boolean;
+};
 
-// compose modal component
-export const Modal: FunctionComponent<ModalProps> & ModalComposition = ({
-  open,
-  setOpen,
-  size,
-  children,
-}: ModalProps) => {
+type BodyProps = Omit<ModalProps, 'open' | 'setOpen'>;
+
+type ModalComposition = {
+  Header: typeof Header;
+  Body: typeof Body;
+  Footer: typeof Footer;
+};
+
+export const Modal: FunctionComponent<ModalProps> & ModalComposition = (props) => {
+  const { open, setOpen, steady, children } = props;
+
+  const [shake, setShake] = useState(false);
+
+  const onClose = () => (steady ? setShake(true) : setOpen(false));
+
   return (
-    <>
-      <Transition appear show={open} as={Fragment}>
-        <Dialog as="div" className="relative z-50" onClose={() => setOpen(false)}>
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-black bg-opacity-25" />
-          </Transition.Child>
+    <LazyMotion features={domAnimation}>
+      <AnimatePresence>
+        {open && (
+          <Dialog as="div" open={open} onClose={onClose} className="relative z-50">
+            <Dialog.Overlay
+              as={m.div}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className={overlayClass()}
+              aria-hidden="true"
+            />
 
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className={modalHeightStyles(size)}>
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
+            <div className={panelContainerClass(props)}>
+              <Dialog.Panel
+                as={m.div}
+                initial={{ opacity: 0, y: 50 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -100, transition: { duration: 0.25 } }}
+                onAnimationEnd={() => setShake(false)}
+                className={panelClass(props, shake)}
               >
-                <Dialog.Panel className={modalWidthStyles(size)}>
-                  <ModalContext.Provider value={{ open, setOpen }}>
-                    <div className="h-full w-full flex flex-col">{children}</div>
-                  </ModalContext.Provider>
-                </Dialog.Panel>
-              </Transition.Child>
+                <div className={childrenContainer()}>{children}</div>
+              </Dialog.Panel>
             </div>
-          </div>
-        </Dialog>
-      </Transition>
-    </>
+          </Dialog>
+        )}
+      </AnimatePresence>
+    </LazyMotion>
   );
 };
 
-// header component
-const Header: FunctionComponent<HeaderProps> = ({ children, withCloseBtn }) => {
-  const { setOpen } = useContext(ModalContext);
+const Header: FunctionComponent<HeaderProps> = (props) => {
+  const { children } = props;
 
-  return (
-    <header className={modalHeaderStyles(withCloseBtn)}>
-      {children}
-      {withCloseBtn && <Close onClick={() => setOpen(false)} size="sm" />}
-    </header>
-  );
+  return <header className={headerClass(props)}>{children}</header>;
 };
 
-// title component
-const Title: FunctionComponent<Props> = ({ children }) => {
-  return <div>{children}</div>;
+const Body: FunctionComponent<BodyProps> = (props) => {
+  const { children } = props;
+
+  return <main className={bodyClass(props)}>{children}</main>;
 };
 
-// body component
-const Body: FunctionComponent<BodyProps> = ({ scrollable, children }) => {
-  return <main className={modalBodyStyles(scrollable)}>{children}</main>;
+const Footer: FunctionComponent<Props> = (props) => {
+  const { children } = props;
+
+  return <footer className={footerClass(props)}>{children}</footer>;
 };
 
-// footer component
-const Footer: FunctionComponent<FooterProps> = ({ alignEnd, children }) => {
-  return <footer className={modalFooterStyles(alignEnd)}>{children}</footer>;
-};
-
-// compose modal with header component
 Modal.Header = Header;
 
-// compose modal with title component
-Modal.Title = Title;
-
-// compose modal with body component
 Modal.Body = Body;
 
-// compose modal with footer component
 Modal.Footer = Footer;
 
-Header.defaultProps = {
-  withCloseBtn: false,
-};
-
-Body.defaultProps = {
-  scrollable: false,
-};
-
-Footer.defaultProps = {
-  alignEnd: false,
-};
-
 Modal.defaultProps = {
-  open: false,
-  size: 'md',
+  size: 'sm',
+  fixedHeight: true,
+  steady: false,
 };
